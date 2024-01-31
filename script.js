58154963b687d0ae2c085e0982c7f71f3f634d7f
Vetha Dharshilin <merlinshylaja25@gmail.com>
2024-01-31 16:53:01 +0530

    JS note

---
+++ script.js
@@ -0,0 +1,9 @@
+// XML-HTTP Methods :
+// Purpose : it is used to interact with the 
+// servers via API
+
+//Total there are 5 steps:
+// Step 01: 
+  var request = new XMLHttpRequest();
+// Step 02 :
+request.open("GET","https://restcountries.com/v3.1/all")
