0d6eec0d9ecaa44a2ea98d97b92c9cec079098e4
Vetha Dharshilin <merlinshylaja25@gmail.com>
2024-02-01 20:41:05 +0530

    update notes

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
 //Total there are 5 steps:
 // Step 01: 
   var request = new XMLHttpRequest();
-// Step 02 :
+// Step 02 open a request :
 request.open("GET","https://restcountries.com/v3.1/all")
+//Step 03 kickstart a request: 
+request.send();
+//step 04:
+// once the data successfully received from the server
+//server status code is 200
+//data coming from the server is the type of string
+//convert string to object
+request.onload = function(){
+  var res = JSON.parse(request.response);
+  console.log(res);
+}
